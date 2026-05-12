794edd3d41b56e09250486eda0dc6fe4047e7a48
chanc-ed <eduardoliscano.aluno@unipampa.edu.br>
2026-05-12 19:51:00 -0300

    Criação dos eventos
    Falta fazer com que eles sejam adicionados a uma lista quando são criados, a exibição e a remoção de eventos

--- Experiencia.java
+++ Experiencia.java
@@ -1,9 +1,51 @@
 import java.util.Date;
 
-public class Experiencia {
+public abstract class Experiencia {
     private String titulo;
     private String descricao;
-    private Date datahora;
+    private Date dataHora;
     private int capacidadeMax;
     private int precoBase;
+
+    public abstract void criarExperiencia();
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(Date dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public int getCapacidadeMax() {
+        return capacidadeMax;
+    }
+
+    public void setCapacidadeMax(int capacidadeMax) {
+        this.capacidadeMax = capacidadeMax;
+    }
+
+    public int getPrecoBase() {
+        return precoBase;
+    }
+
+    public void setPrecoBase(int precoBase) {
+        this.precoBase = precoBase;
+    }
 }
--- Workshop.java
+++ Workshop.java
@@ -1,4 +1,40 @@
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Workshop extends Experiencia {
-    private String pontoEncontro;
-    private String nomeGuia;
+    private String ingredientesEMateriais;
+
+    public String getIngredientesEMateriais() {
+        return ingredientesEMateriais;
+    }
+
+    public void setIngredientesEMateriais(String ingredientesEMateriais) {
+        this.ingredientesEMateriais = ingredientesEMateriais;
+    }
+
+    @Override
+    public void criarExperiencia(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o nome do evento: ");
+        String titulo = scan.nextLine();
+        System.out.println("Digite uma breve descrição do evento: ");
+        String descricao = scan.nextLine();
+        System.out.println("Digite a data e hora do evento: ");
+        String stringDataHora = scan.nextLine();
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        Date dataHora = (Date) formatador.parse(stringDataHora);
+        System.out.println("Digite a capacidade máxima de pessoas do evento: ");
+        int capacidadeMax = scan.nextInt();
+        System.out.println("Digite o preço base da entrada: ");
+        int preco = scan.nextInt();
+        System.out.println("Digite os ingredientes e materiais que são necessários serem levados: ");
+        String ingredientesEMateriais = scan.nextLine();
+        setTitulo(titulo);
+        setDescricao(descricao);
+        setDataHora(dataHora);
+        setCapacidadeMax(capacidadeMax);
+        setPrecoBase(preco);
+        setIngredientesEMateriais(ingredientesEMateriais);
+    }
 }
