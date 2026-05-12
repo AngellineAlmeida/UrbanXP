11f8bdef3fe57541048ffd4efd99d61bff4da46d
Murillol <murilloroque.aluno@unipampa.edu.br>
2026-05-12 16:50:13 -0300

    DescontoPremium.java
    criação do desconto premium

---
+++ DescontoPremium.java
@@ -0,0 +1,7 @@
+public class DescontoPremium implements Desconto {
+    @Override
+    public double calcularPrecoFinal(double precoBase) {
+        double valorFinal = precoBase - 30.0;
+        return (valorFinal > 0) ? valorFinal : 0;
+    }
+}
