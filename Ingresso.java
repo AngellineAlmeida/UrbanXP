4ae62aa6c0e71d437aaf336b5963efb2894d7eb3
Murillol <murilloroque.aluno@unipampa.edu.br>
2026-05-14 12:53:00 -0300

    Update Ingresso.java
    reorganização da classe ingresso

--- Ingresso.java
+++ Ingresso.java
@@ -1,55 +1,31 @@
-enum Estado{
-    Reservado,
-    Pago,
-    Cancelado
-}
+package dominio_urbanxp;
 
 public class Ingresso {
     private Estado estado;
-    private String nome;
-    private long cpf;
+    private Cliente cliente;
     private Experiencia evento;
 
-    public void criarIngresso(String nome, long cpf, Experiencia evento){
-        setNome(nome);
-        setCpf(cpf);
-        setEvento(evento);
-        setEstado(Estado.Reservado);
-    }
-
-    public void mostrarIngresso(){
-        System.out.println("--Informações do ingresso--\nNome: " + getNome() + "\nCPF: " + getCpf() + "\nEvento: " + evento.getTitulo() + "\nEstado: " + getEstado());
-    }
-
-    public Estado getEstado() {
-        return estado;
-    }
-
-    public void setEstado(Estado estado) {
-        this.estado = estado;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public long getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(long cpf) {
-        this.cpf = cpf;
-    }
-
-    public Experiencia getEvento() {
-        return evento;
-    }
-
-    public void setEvento(Experiencia evento) {
+    public Ingresso(Cliente cliente, Experiencia evento) {
+        this.cliente = cliente;
         this.evento = evento;
+        this.estado = Estado.RESERVADO;
     }
+
+    public void mostrarIngresso() {
+        double valorFinal = cliente.calcularValorFinal(evento.getPrecoBase());
+        System.out.println("\n-------------------------------------------");
+        System.out.println("            INGRESSO URBANXP               ");
+        System.out.println("-------------------------------------------");
+        System.out.println("Cliente: " + cliente.getNome());
+        System.out.println("Evento: " + evento.getTitulo());
+        System.out.println("Data/Hora: " + evento.getDataHora());
+        System.out.println("Status Atual: " + estado);
+        System.out.println(String.format("Preço Pago: R$ %.2f", valorFinal));
+        System.out.println("-------------------------------------------\n");
+    }
+
+    public Estado getEstado() { return estado; }
+    public void setEstado(Estado estado) { this.estado = estado; }
+    public Cliente getCliente() { return cliente; }
+    public Experiencia getEvento() { return evento; }
 }
